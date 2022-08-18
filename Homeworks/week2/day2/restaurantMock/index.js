1ddb0b03205fadc5202b6f8746ed04566a7a8d66
ZOZO-coding <zyken9054@gmail.com>
2022-08-18 09:26:10 -0400

    second res mock

--- Homeworks/week2/day2/restaurantMock/index.js
+++ Homeworks/week2/day2/restaurantMock/index.js
@@ -2,6 +2,17 @@ const feedback = document.querySelector("#feedback");
 const input = document.querySelector("#contactInfo");
 const div = document.querySelector("#feedbackInfo");
 
+const breakfastMenu = document.querySelector('.breakfast');
+
+const menu = {
+    brunch: ["english breakfast", "bread and butter", "salad"],
+    lunch: ["pasta", "fried egg", "ramen", "tantan noodle"],
+    dinner: ["steak", "fried chicken", "surf and turf"],
+    drinks: ["mimosa", "whiskey", "bloody marry", "sangria", "coffee"]
+}
+
+const {brunch, lunch, dinner, drinks} = menu;
+
 feedback.addEventListener("submit", function(e) {
     e.preventDefault();
     const value = input.value;
@@ -9,3 +20,9 @@ feedback.addEventListener("submit", function(e) {
     msg.innerText = value;
     div.append(msg);
 })
+
+for (let bru of brunch) {
+    const brunchItem = document.createElement("p")
+    brunchItem.innerText = bru
+    div.append(brunchItem)
+}
