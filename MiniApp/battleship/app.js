0a9ed98b2c0e2b9cb80831a7ee6e5e27ff5268e0
Yu <zyken9054@gmail.com>
2022-08-29 15:44:12 -0400

    second version: got more enemies to display, trying to improve the functionality to that my ship can attack any enemies

--- MiniApp/battleship/app.js
+++ MiniApp/battleship/app.js
@@ -1,8 +1,13 @@
 import { PlayerShip, EnemyShip } from "./classes.js";
+import {enemy2, enemy3, enemy4, enemy5, enemy6} from "./enemyship.js";
+// create which turn indicator:
+let yourTurn;
+
 
 // create objects of you and enemy
 const yourShip = new PlayerShip('zoey');
 const enemy1 = new EnemyShip('enemy no.1');
+const enemies = [enemy2, enemy3, enemy4, enemy5, enemy6];
 
 // create elements
 const playerAttackBtn = document.getElementById('player-attack');
@@ -17,6 +22,26 @@ const enemyHull = document.getElementById('enemy-hull');
 const enemyFirePower = document.getElementById('enemy-firepower');
 const enemyAccuracy = document.getElementById('enemy-accuracy');
 
+const enemyTerritory = document.querySelector('.enemy-ships');
+
+for (let i = 0; i < enemies.length; i++) {
+    const enemyContainer = document.createElement('div');
+    const enemyName = document.createElement('h2');
+    enemyName.innerText = `Enemy ${i}`;
+    const enemyBtn = document.createElement('button');
+    enemyBtn.innerText = 'attack';
+    const statusReport = document.createElement('h4')
+    statusReport.innerText = 'Status Report';
+    const enemyHull = document.createElement('div')
+    enemyHull.innerText = 'Enemy Hull: ' +  enemies[i].hull;
+    const enemyFirePower = document.createElement('div')
+    enemyFirePower.innerText = 'Enemy FirePower: ' +  enemies[i].firepower;
+    const enemyAccuracy = document.createElement('div')
+    enemyAccuracy.innerText = 'Enemy Accuracy: ' +  enemies[i].accuracy;
+    
+    enemyTerritory.appendChild(enemyContainer).appendChild(enemyName).appendChild(statusReport).appendChild(enemyHull).appendChild(enemyFirePower).appendChild(enemyAccuracy).append(enemyBtn);
+}
+
 // create html elements for displaying status of player and enemy
 console.log(yourShip);
 playerHull.append(`${yourShip.hull}`);
@@ -26,7 +51,6 @@ playerAccuracy.append(`${yourShip.accuracy}`);
 enemyHull.append(`Enemy Hull: ${enemy1.hull}`);
 enemyFirePower.append(`Enemy FirePower: ${enemy1.firepower}`);
 enemyAccuracy.append(`Enemy Accuracy: ${enemy1.accuracy}`);
-
 
 // player actions
 
@@ -52,6 +76,18 @@ function enemyAttack() {
     playerHull.innerText = '';
     playerHull.append(`Your Hull: ${yourShip.hull}`);
     if (yourShip.isDead) {
-        alert('you got destroyed by an enemy ship')
+        
     }
+}
+
+function swapTurns() {
+    yourTurn = !yourTurn;
+}
+
+function ceaseFireBtn(btn) {
+    btn.setAttribute("disabled", "");
+}
+
+function openFireBtn(btn) {
+    btn.removeAttribute("disabled");
 }
---
+++ MiniApp/battleship/enemyship.js
@@ -0,0 +1,7 @@
+import { EnemyShip } from "./classes.js";
+
+export const enemy2 = new EnemyShip('enemy2');
+export const enemy3 = new EnemyShip('enemy2');
+export const enemy4 = new EnemyShip('enemy2');
+export const enemy5 = new EnemyShip('enemy2');
+export const enemy6 = new EnemyShip('enemy2');
