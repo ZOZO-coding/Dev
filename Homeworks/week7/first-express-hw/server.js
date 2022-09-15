03a2df1aedcf6da811faea64fccd4eca9c3ba998
Yu <zyken9054@gmail.com>
2022-09-15 17:42:04 -0400

    Tip calculator express application created

--- Homeworks/week7/first-express-hw/server.js
+++ Homeworks/week7/first-express-hw/server.js
@@ -6,6 +6,12 @@ app.get('/greeting/:name', (req, res) => {
     res.send(`Welcome back, ${req.params.name}`)
 })
 
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    const {total, tipPercentage} = req.params;
+    const tip = parseFloat(total) * parseFloat(tipPercentage) / 100;
+    res.send(tip.toString());
+})
+
 app.listen(3000, () => {
     console.log('listening to port 3000');
 })
