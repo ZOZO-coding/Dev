fc0d69b731152914fa4be5a43f72c48df2a070aa
Yu <zyken9054@gmail.com>
2022-09-15 20:15:20 -0400

    Magic 8 Ball express application created

--- Homeworks/week7/first-express-hw/server.js
+++ Homeworks/week7/first-express-hw/server.js
@@ -12,6 +12,17 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
     res.send(tip.toString());
 })
 
+app.get('/magic/:phrase', (req, res) => {
+    const {phrase} = req.params;
+    const magic8Res = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+    const randomNum = Math.floor(Math.random() * magic8Res.length);
+    if (phrase === 'Will I be a Millionaire') {
+        res.send(`<h1>${magic8Res[randomNum]}</h1>`)
+    } else {
+        res.send('<h1>I dont understand this question</h1>')
+    }
+})
+
 app.listen(3000, () => {
     console.log('listening to port 3000');
 })
