7e244ff67b95967c17f7706c12a66ccb7c7608e4
Yu <zyken9054@gmail.com>
2022-09-18 17:33:42 -0400

    connected my database, can see json in the browser

--- Homeworks/week7/poke-express/server.js
+++ Homeworks/week7/poke-express/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const pokemon = require('./models/pokemon');
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Pokemon App!')
+})
+
+app.get('/pokemon', (req, res) => {
+    res.send(pokemon);
 })
 
 
