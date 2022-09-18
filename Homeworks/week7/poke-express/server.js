9726ac3c489c3d93a99b515d9294af36a8f24ecf
Yu <zyken9054@gmail.com>
2022-09-18 17:42:31 -0400

    index.jsx view rendered at pokemon route

--- Homeworks/week7/poke-express/server.js
+++ Homeworks/week7/poke-express/server.js
@@ -3,12 +3,15 @@ const pokemon = require('./models/pokemon');
 
 const app = express();
 
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+
 app.get('/', (req, res) => {
     res.send('Welcome to the Pokemon App!')
 })
 
 app.get('/pokemon', (req, res) => {
-    res.send(pokemon);
+    res.render('Index');
 })
 
 
